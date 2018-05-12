7213fd8f3e763463953efab7adba8ffe2d6816f5
Linh Phan <linhphanhust@gmail.com>
2018-05-12 17:00:53 +0700

    add find best face

--- facerecog.py
+++ facerecog.py
@@ -61,44 +61,44 @@ def train_and_save_model(input_folder):
 
 def predict_img(img, count):
     
-    eye_cascade = cv2.CascadeClassifier("model/haarcascade_lefteye_2splits.xml") # haarcascade_eye_tree_eyeglasses haarcascade_lefteye_2splits
+    eye_cascade = cv2.CascadeClassifier("model/haarcascade_eye_tree_eyeglasses.xml") 
+    # haarcascade_eye_tree_eyeglasses haarcascade_lefteye_2splits haarcascade_eye
+
     face_positions, list_face_gray = face_dectect(img)
-    
+
     for (x,y,w,h) , face_gray in zip(face_positions, list_face_gray):
         face_color = img[y:y+h, x:x+w]
         face_crop = cv2.resize(face_gray, (64,64))
         label, confidence = face_recognizer.predict(face_crop)
-        print("face: ", x,y,w,h)
-        eyes = eye_cascade.detectMultiScale(face_gray)
-
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(face_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
-            print("eyes: ",ex, ey, ew, eh)
-        
         
         if(confidence<170):
             text = label_names[label] + " " + str(int(confidence))
-            if(count%6==0):
-                if not os.path.exists("output/"+label_names[label] +"/"):
-                    os.makedirs("output/"+label_names[label] +"/")
-                cv2.imwrite("output/"+label_names[label] +"/"+label_names[label]+str(int(confidence)) + ".jpg",face_color)
+            
+            if not os.path.exists("output/"+label_names[label] +"/"):
+                os.makedirs("output/"+label_names[label] +"/")
+            file_name = "output/"+label_names[label] +"/"+label_names[label] +"-" + str(int(confidence)) +"-" + str(count)+ ".jpg"
+            cv2.imwrite(file_name,face_color)
         else:
             text = "Unknown " + str(int(confidence))
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
         cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 0, 0), 2)
     
     cv2.imshow("img",img)
+    cv2.imwrite("face.jpg",img)
+
+    # return min_confidence
     
 def predict_camera():
     
     # fps = int(capture.get(cv2.CAP_PROP_FPS))
-    fps = 2
+    frame_per_cap = 2
     count = 0
     capture = cv2.VideoCapture(0) # "input/aslongas.mp4"
     while(capture.isOpened()):
         ret, frame = capture.read()
-        if(count%fps ==0):
+        if(count%frame_per_cap ==0):
             predict_img(frame, count)
+
         count+=1
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -106,15 +106,58 @@ def predict_camera():
     cv2.destroyAllWindows()
 
 
+def find_best_face(face_folder, range_conf):
+    
+    eye_cascade = cv2.CascadeClassifier("model/haarcascade_eye_tree_eyeglasses.xml") 
+    # haarcascade_eye_tree_eyeglasses haarcascade_lefteye_2splits haarcascade_eye
+    
+    list_file = os.listdir(face_folder)
+    min_straight_score = 1000
+    best_face = list_file[0]
+    threshold_confidence = int(list_file[0].split("-")[1]) + range_conf
+
+    for imgfile in list_file:
+        confidence = imgfile.split("-")[1]
+        confidence = int(confidence)
+        if confidence > threshold_confidence:
+            continue
+        img = cv2.imread(face_folder + imgfile)
+        face_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face_crop = cv2.resize(face_gray,(100,100))
+
+        eyes = eye_cascade.detectMultiScale(face_crop)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(face_crop,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+        
+        if (isinstance(eyes, tuple) or eyes.shape != (2,4) ):
+                continue
+        
+        straight_score = 1000
+        straight_score = abs(eyes[1][0] -eyes[0][0]) + abs(eyes[0][0] + eyes[1][0] +eyes[1][2] - 100)
+        if(straight_score< min_straight_score):
+            min_straight_score = straight_score
+            best_face = imgfile
+        
+    
+    print("best face: ", best_face, straight_score, min_straight_score)
+    face = cv2.imread("output/Linh/"+ best_face)
+    cv2.imshow("best face", face)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()  # EigenFaceRecognizer_create() or FisherFaceRecognizer_create()
-# train_and_save_model("train/")
+train_and_save_model("train/")
 
 if not label_names:
     label_names = ["Nga", "Linh", "JVmind","Tiffany"]
 
 face_recognizer.read("model/lbph_model.yml")
 predict_camera()
+find_best_face("output/Linh/", 20)
 
-# predict_img(imgfile)
-# imgfile = cv2.imread("linh.jpg")
+
+# imgfile = cv2.imread("12.jpg")
+# predict_img(imgfile, 1)
 
